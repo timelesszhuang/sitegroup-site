93b2daa593eb57a2a0bab9f61208c34b1ef0a356
timelessz <834916321@qq.com>
2017-09-07 16:44:45 +0800

    # 修改首页样式

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -9,7 +9,7 @@
   .layout-logo-left {
     width: 100%;
     background-color: #ffffff;
-    height: 60px;
+    height: 50px;
   }
 
   .layout-breadcrumb {
@@ -53,6 +53,13 @@
     color: #ffffff;
   }
 
+  .logo {
+    width: 150px;
+    display: block;
+    padding: 10px;
+    margin-left: 10%;
+  }
+
   /*字体改为白色*/
   .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item, .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu-title {
     color: #ffffff;
@@ -77,11 +84,9 @@
     float: right;
     z-index: 999;
     position: fixed;
-    top: 0;
     left: 16.7%;
-    height: 60px;
+    height: 50px;
     background: #fff;
-    box-shadow: 0 1px 1px rgba(0, 0, 0, .1);
   }
 
   .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item-active:not(.ivu-menu-submenu), .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu-title-active:not(.ivu-menu-submenu) {
@@ -98,114 +103,149 @@
   .ivu-breadcrumb {
     color: #999;
     font-size: 14px;
-    padding-top: 4.5%;
-  }
-
-  .Site {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
+    padding-top: 4%;
   }
 
   .layout-hide-text, .layout-text {
     display: inline-block;
-    width: 70%;
+    width: 100%;
+  }
+
+  .site {
+    width: 100%;
+  }
+
+  /**底部版权信息*/
+  .layout-copy {
+    position: fixed;
+    text-align: center;
+    padding: 5px 0 10px;
+    bottom: 0px;
+    height: 40px;
+    width: 84%;
+    background-color: #fff;
   }
 
   .parent-menu-title {
     color: #ffffff !important;
   }
 
-
 </style>
 <template>
   <div class="layout">
-
     <Row type="flex">
       <i-col span="4" class="layout-menu-left">
         <Menu active-name="activename" theme="dark" width="auto" :open-names="['1']">
           <div class="layout-logo-left">
-            <img style="" src="../../src/assets/logo.png" alt="">
+            <img class="logo" style="" src="../../src/assets/logo.png" alt="乐销易创意发布平台">
           </div>
           <template slot="title">
             <Icon type="ios-keypad"></Icon>
             管理
           </template>
           <Menu-item name="微信公众号文章管理">
-            <Icon type="settings"></Icon>
-            <span class="layout-text" @click="routerChange('/common/weixinarticle','微信公众号文章管理')">微信公众号文章管理</span>
+            <span class="layout-text" @click="routerChange('/common/weixinarticle','微信公众号文章管理')">
+              <Icon type="settings"></Icon>
+              微信公众号文章管理
+            </span>
           </Menu-item>
           <Menu-item name="新闻文章管理">
-            <Icon type="ios-bookmarks"></Icon>
-            <span class="layout-text" @click="routerChange('/common/news','新闻文章管理')">新闻文章管理</span>
+            <span class="layout-text" @click="routerChange('/common/news','新闻文章管理')">
+              <Icon type="ios-bookmarks"></Icon>
+              新闻文章管理
+            </span>
           </Menu-item>
           <Menu-item name="私有文章库">
+            <span class="layout-text" @click="routerChange('/common/article','私有文章库')">
             <Icon type="ios-book-outline"></Icon>
-            <span class="layout-text" @click="routerChange('/common/article','私有文章库')">私有文章库</span>
+              私有文章库
+            </span>
           </Menu-item>
           <Menu-item name="文章a链接">
-            <Icon type="wrench"></Icon>
-            <span class="layout-text" @click="routerChange('/common/href','文章插入链接')">文章插入链接</span>
+            <span class="layout-text" @click="routerChange('/common/href','文章插入链接')">
+              <Icon type="wrench"></Icon>
+              文章插入链接
+            </span>
           </Menu-item>
           <Menu-item name="关键词链接替换">
+            <span class="layout-text" @click="routerChange('/common/hrefreplace','关键词链接替换')">
             <Icon type="wrench"></Icon>
-            <span class="layout-text" @click="routerChange('/common/hrefreplace','关键词链接替换')">关键词链接替换</span>
+              关键词链接替换
+            </span>
           </Menu-item>
           <Menu-item name="文章关键词替换">
+            <span class="layout-text" @click="routerChange('/common/substitution','文章关键词替换')">
             <Icon type="ios-gear"></Icon>
-            <span class="layout-text" @click="routerChange('/common/substitution','文章关键词替换')">文章关键词替换</span>
+              文章关键词替换
+            </span>
           </Menu-item>
           <Menu-item name="页面生成">
-            <Icon type="ios-cog"></Icon>
-            <span class="layout-text" @click="operation()">页面生成</span>
+            <span class="layout-text" @click="operation()">
+              <Icon type="ios-cog"></Icon>
+              页面生成
+            </span>
           </Menu-item>
           <Menu-item name="搜索引擎优化管理">
-            <Icon type="settings"></Icon>
-            <span class="layout-text" @click="routerChange('/common/tdkall','搜索引擎优化管理')">搜索引擎优化管理</span>
+            <span class="layout-text" @click="routerChange('/common/tdkall','搜索引擎优化管理')">
+              <Icon type="settings"></Icon>
+              搜索引擎优化管理
+            </span>
           </Menu-item>
-          <!--<Menu-item name="tdk">-->
-          <!--<Icon type="settings"></Icon>-->
-          <!--<span class="layout-text" @click="routerChange('/common/articletdk','tdk')">tdk</span>-->
-          <!--</Menu-item>-->
           <Menu-item name="静态化配置">
-            <Icon type="android-options"></Icon>
-            <span class="layout-text" @click="routerChange('/common/staticconfig','静态化配置')">静态化配置</span>
+            <span class="layout-text" @click="routerChange('/common/staticconfig','静态化配置')">
+              <Icon type="android-options"></Icon>
+              静态化配置
+            </span>
           </Menu-item>
           <Menu-item name="关键词占比">
-            <Icon type="android-cloud-circle"></Icon>
-            <span class="layout-text" @click="routerChange('/common/keyword','关键词占比')">关键词占比统计</span>
+            <span class="layout-text" @click="routerChange('/common/keyword','关键词占比')">
+              <Icon type="android-cloud-circle"></Icon>
+              关键词占比统计
+            </span>
           </Menu-item>
           <Menu-item name="浏览量统计">
-            <Icon type="settings"></Icon>
-            <span class="layout-text" @click="routerChange('/common/pv','浏览量统计')">浏览量统计</span>
+            <span class="layout-text" @click="routerChange('/common/pv','浏览量统计')">
+              <Icon type="ios-monitor-outline"></Icon>
+              浏览量统计
+            </span>
           </Menu-item>
           <Menu-item name="浏览量展示">
-            <Icon type="ios-monitor-outline"></Icon>
-            <span class="layout-text" @click="routerChange('/common/show','浏览量展示')">浏览量展示</span>
+            <span class="layout-text" @click="routerChange('/common/show','浏览量展示')">
+              <Icon type="ios-monitor-outline"></Icon>
+              浏览量展示
+            </span>
           </Menu-item>
           <Menu-item name="爬虫统计">
-            <Icon type="ios-book-outline"></Icon>
-            <span class="layout-text" @click="routerChange('/common/crawler','爬虫统计')">爬虫统计</span>
+            <span class="layout-text" @click="routerChange('/common/crawler','爬虫统计')">
+              <Icon type="ios-book-outline"></Icon>
+              爬虫统计
+            </span>
           </Menu-item>
           <Menu-item name="搜索引擎占比">
-            <Icon type="monitor"></Icon>
-            <span class="layout-text" @click="routerChange('/common/flow','搜索引擎占比')">搜索引擎占比</span>
+            <span class="layout-text" @click="routerChange('/common/flow','搜索引擎占比')">
+              <Icon type="monitor"></Icon>
+              搜索引擎占比
+            </span>
           </Menu-item>
           <Menu-item name="重置密码">
           </Menu-item>
         </Menu>
       </i-col>
-      <i-col style="position: relative;overflow: auto" span="20">
+      <i-col style="overflow-y:scroll;padding-bottom: 50px" span="20">
         <div class="layout-header">
           <Row type="flex" justify="end" align="middle" class="code-row-bg">
             <Col span="12" align="left" style="cursor: pointer">
             <span class="layout-text" style="font-family:Microsoft YaHei;font-size: 15px;"
-                  @click="routerChange('/common/count')"> <Icon type="home" style="font-size: 15px"
-                                                                @click="routerChange('/common/count')"></Icon>首页</span>
+                  @click="routerChange('/common/count')">
+              <Icon type="home" style="font-size: 15px" @click="routerChange('/common/count')"></Icon>
+              首页
+            </span>
             </Col>
             <Col span="4" align="right" style="cursor: pointer">
             <Icon type="paper-airplane"></Icon>
-            <a v-bind:href="url" target="_blank" title="点此预览此网站" style="font-size: 15px;">{{SiteName}}</a>
+            <a v-bind:href="url" target="_blank" title="点此预览此网站" style="font-size: 15px;">
+              {{SiteName}}
+            </a>
             </Col>
             <Col span="3" align="center">
             <Badge :count="count">
@@ -229,16 +269,16 @@
             <Breadcrumb-item>{{activeName}}</Breadcrumb-item>
           </Breadcrumb>
         </div>
-        <div class="Site">
+        <div class="site">
           <div class="layout-content">
             <div class="layout-content-main">
               <router-view></router-view>
             </div>
           </div>
-          <div class="layout-copy">
-            2015-2017 &copy; 北京易至信科技有限公司
-            <div>京ICP12019481号</div>
-          </div>
+        </div>
+        <div class="layout-copy">
+          2015-2017 &copy; 北京易至信科技有限公司
+          <div>京ICP12019481号</div>
         </div>
       </i-col>
     </Row>
